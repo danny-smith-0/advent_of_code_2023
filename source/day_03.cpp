6367b43ceb416c692c72e7a31559656b509a9e81
Danny Smith <danny.smith.0@outlook.com>
2023-12-04 13:47:32 -0500

    Fixed the bug for part 1

--- source/day_03.cpp
+++ source/day_03.cpp
@@ -15,63 +15,63 @@ sll part1(strings_t input)
         for (size_t jj = 0; jj < num_cols; ++jj)
         {
             char cell = input[ii][jj];
-            if (is_a_number(cell))
+            bool is_number = is_a_number(cell);
+            if (is_number)
             {
                 if (!start_col_opt.has_value())
                     start_col_opt = jj;
                 end_col = jj;
             }
-            else
+
+            bool this_is_the_first_non_number_cell_after_a_number = !is_number && start_col_opt.has_value() && end_col == jj - 1;
+            bool this_is_the_last_column_and_this_cell_is_a_number = is_number && jj == num_cols - 1;
+            if (this_is_the_first_non_number_cell_after_a_number || this_is_the_last_column_and_this_cell_is_a_number)
             {
-                // If we just found the first non-number cell after 1 or more number cells
-                if (start_col_opt.has_value() && end_col == jj - 1)
+                size_t start_col = start_col_opt.value();
+                int the_number = std::stoi(input[ii].substr(start_col, jj - start_col + 1));
+                if (isnt_a_number_nor_period(cell))
                 {
-                    size_t start_col = start_col_opt.value();
-                    int the_number = std::stoi(input[ii].substr(start_col, jj - start_col + 1));
-                    if (cell != '.')
-                    {
-                        // If the current cell isn't a period nor a number, it must be a symbol. Add the number
-                        sum += the_number;
-                    }
-                    else if (start_col > 0 && isnt_a_number_nor_period(input[ii][start_col - 1]))
-                    {
-                        // If there is one, check the cell on this row before the number started
-                        sum += the_number;
-                    }
-                    else
-                    {
-                        // Check all adjacent cells in rows before and after this row, but don't go off the end of the matrix
-                        ints_t row_indices_to_check;
-                        if (ii > 0)
-                            row_indices_to_check.push_back(static_cast<int>(ii) - 1);
-                        if (ii < num_rows - 1)
-                            row_indices_to_check.push_back(static_cast<int>(ii) + 1);
+                    // If the current cell isn't a period nor a number, it must be a symbol. Add the number
+                    sum += the_number;
+                }
+                else if (start_col > 0 && isnt_a_number_nor_period(input[ii][start_col - 1]))
+                {
+                    // If there is one, check the cell on this row before the number started
+                    sum += the_number;
+                }
+                else
+                {
+                    // Check all adjacent cells in rows before and after this row, but don't go off the end of the matrix
+                    ints_t row_indices_to_check;
+                    if (ii > 0)
+                        row_indices_to_check.push_back(static_cast<int>(ii) - 1);
+                    if (ii < num_rows - 1)
+                        row_indices_to_check.push_back(static_cast<int>(ii) + 1);
 
-                        int start_index_to_check = static_cast<int>(start_col > 0         ? start_col - 1 :            0);
-                        int last_index_to_check = static_cast<int>(end_col < num_cols - 1 ?   end_col + 1 : num_cols - 1);
-                        ints_t col_indices_to_check = aoc::matlab_colon(start_index_to_check, last_index_to_check);
+                    int start_index_to_check = static_cast<int>(start_col > 0         ? start_col - 1 :            0);
+                    int last_index_to_check = static_cast<int>(end_col < num_cols - 1 ?   end_col + 1 : num_cols - 1);
+                    ints_t col_indices_to_check = aoc::matlab_colon(start_index_to_check, last_index_to_check);
 
-                        bool found_a_symbol = false;
-                        for (auto row_index : row_indices_to_check)
+                    bool found_a_symbol = false;
+                    for (auto row_index : row_indices_to_check)
+                    {
+                        for (auto col_index : col_indices_to_check)
                         {
-                            for (auto col_index : col_indices_to_check)
+                            if (isnt_a_number_nor_period(input[row_index][col_index]))
                             {
-                                if (isnt_a_number_nor_period(input[row_index][col_index]))
-                                {
-                                    found_a_symbol = true;
-                                    break;
-                                }
-                            }
-                            if (found_a_symbol)
-                            {
-                                sum += the_number;
+                                found_a_symbol = true;
                                 break;
                             }
                         }
+                        if (found_a_symbol)
+                        {
+                            sum += the_number;
+                            break;
+                        }
                     }
-                    start_col_opt.reset();
-                    end_col = 0;
                 }
+                start_col_opt.reset();
+                end_col = 0;
             }
         }
     }
@@ -102,11 +102,13 @@ int main ()
         // 600000 is too high
         // 540000 is too low
         // If I didn't skip ANY numbers the total would be 597389. Should've checked this before guessing 600000
+        // I was missing numbers along the right column. Fixed. New guess: 541515. Wrong. No longer gives hints
+        // Mmm. I was handling those final columns incorrectly, adding all. New guess: 540131
     }
 
     std::cout << "\nPart 2\n\n";
     sll results_test_2 = part2(test_data);
-    sll expected_test_result_2 = -1;
+    sll expected_test_result_2 = 467835;
     if (aoc::results(results_test_2, expected_test_result_2))
     {
         sll results_real_2 = part2(real_data);
